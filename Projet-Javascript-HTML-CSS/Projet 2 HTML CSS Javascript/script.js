ef5ec6b9b896b9941c72db472223a9b3e519b173
YacineBoukliff <169815647+YacineBoukliff@users.noreply.github.com>
2024-07-28 16:57:34 +0200

    Ajout slider carte librairie Swiper JS

--- Projet-Javascript-HTML-CSS/Projet 2 HTML CSS Javascript/script.js
+++ Projet-Javascript-HTML-CSS/Projet 2 HTML CSS Javascript/script.js
@@ -1,53 +1,69 @@
 document.addEventListener("DOMContentLoaded", () => {
-  const salahButton = document.querySelector(".js-button");
+  const salahButtons = document.querySelectorAll(".js-button");
 
-  salahButton.addEventListener("click", () => {
-    if (salahButton.innerText === "Mohamed Salah") {
-      salahButton.innerText = "The Egyptian King";
-      salahButton.style.fontSize = "24px";
-      salahButton.style.background = "black";
-      salahButton.style.color = "red";
-    } else {
-      salahButton.innerText = "Mohamed Salah";
-      salahButton.style.fontSize = "16px";
-      salahButton.style.background = "red";
-      salahButton.style.color = "white";
-    }
+  salahButtons.forEach(salahButton => {
+    salahButton.addEventListener("click", () => {
+      if (salahButton.innerHTML === "Mohamed Salah") {
+        salahButton.innerText = "The Egyptian King";
+        salahButton.style.fontSize = "24px";
+        salahButton.style.background = "black";
+        salahButton.style.color = "red";
+      } else {
+        salahButton.innerText = "Mohamed Salah";
+        salahButton.style.fontSize = "16px";
+        salahButton.style.background = "red";
+        salahButton.style.color = "white";
+      }
+    });
   });
 });
 
-document.addEventListener("DOMContentLoaded", () => {
-  const salahButton = document.querySelector(".js-button1");
 
-  salahButton.addEventListener("click", () => {
-    if (salahButton.innerText === "Darwin Nunez") {
-      salahButton.innerText = "Darwin MERGUEZ !!!";
-      salahButton.style.fontSize = "35px";
-      salahButton.style.background = "black";
-      salahButton.style.color = "red";
-    } else {
-      salahButton.innerText = "Darwin Nunez";
-      salahButton.style.fontSize = "16px";
-      salahButton.style.background = "red";
-      salahButton.style.color = "white";
+
+
+const swiper = new Swiper('.swiper', {
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+  
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+  
+  slidesPerView: 3,
+  loop: true,
+
+  autoplay: {
+    delay: 3000,
+    disableOnInteraction: false,
+    pauseOnMouseEnter: true,
+  },
+  speed: 600,
+  breakpoints: {
+
+    320: {
+      slidesPerView: 1,
+      spaceBetween: 10
+    },
+    480: {
+      slidesPerView: 1,
+      spaceBetween: 20
+    },
+    640: {
+      slidesPerView: 1,
+      spaceBetween: 30,
+    },
+    1024: {
+      slidesPerView: 3,
+      spaceBetween: 40,
+    },
+    1366: {
+      slidesPerView: 3,
+      spaceBetween: 50,
     }
-  });
-});
-
-document.addEventListener("DOMContentLoaded", () => {
-  const salahButton = document.querySelector(".js-button2");
-
-  salahButton.addEventListener("click", () => {
-    if (salahButton.innerText === "Cody Gakpo") {
-      salahButton.innerText = "Sous coté";
-      salahButton.style.fontSize = "24px";
-      salahButton.style.background = "black";
-      salahButton.style.color = "red";
-    } else {
-      salahButton.innerText = "Cody Gakpo";
-      salahButton.style.fontSize = "16px";
-      salahButton.style.background = "red";
-      salahButton.style.color = "white";
-    }
-  });
+  },
+  
+  
 });
