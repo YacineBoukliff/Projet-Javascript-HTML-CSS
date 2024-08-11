954fccfbe00ab56279a49effeaa27da0264a0cab
yacine bouklif <y.bouklif@it-students.fr>
2024-08-11 22:32:06 +0200

    Générer le HTML en Javascript ENFIN !!!

--- Projet-Javascript-HTML-CSS/Projet 2 HTML CSS Javascript/script.js
+++ Projet-Javascript-HTML-CSS/Projet 2 HTML CSS Javascript/script.js
@@ -1,63 +1,399 @@
-document.addEventListener("DOMContentLoaded", () => {
-  const salahButtons = document.querySelectorAll(".js-button");
+const JoueurInfo = {
+  "Mohamed Salah": {
+    Surnom: "The Egyptian King",
+    color: "red",
+  },
+  "Darwin Nunez": {
+    Surnom: "La Resiliencia",
+    color: "red",
+  },
+  "Cody Gakpo": {
+    Surnom: "The Dutch Dynamo",
+    color: "red",
+  },
+  "Diogo Jota": {
+    Surnom: "Diogoal",
+    color: "red",
+  },
+  "Luiz Diaz": {
+    Surnom: "The Colombian Wizard",
+    color: "red",
+  },
+  Szoboszlai: {
+    Surnom: "The Hungarian Maestro",
+    color: "red",
+  },
+  "Alexis Mac Allister": {
+    Surnom: "El Coloradito",
+    color: "red",
+  },
+  "Wataru Endo": {
+    Surnom: "The Japanese Engine",
+    color: "red",
+  },
+  "Harvey Elliot": {
+    Surnom: "Diamond ",
+    color: "red",
+  },
+  "Ryan Gravenberch": {
+    Surnom: "The Dutch Powerhouse",
+    color: "red",
+  },
+  "Joe Gomez": {
+    Surnom: "JoJoooo",
+    color: "red",
+  },
+  "Van Dijk": {
+    Surnom: "The Best Ever.",
+    color: "red",
+  },
+  "Ibrahima Konate": {
+    Surnom: "IBOUUUU",
+    color: "red",
+  },
+  "Kostantinos Tsimikas": {
+    Surnom: "The Greek Scouser",
+    color: "red",
+  },
+  "Andy Robertson": {
+    Surnom: "The Scottish Assist King",
+    color: "red",
+  },
+  "Trent Alexander Arnold": {
+    Surnom: "TAA",
+    color: "red",
+  },
+  "Jarell Quansah": {
+    Surnom: "The Young Guardian",
+    color: "red",
+  },
+  "Conor Bradley": {
+    Surnom: "The Northern Irish Prospect",
+    color: "red",
+  },
+  "Alisson Becker": {
+    Surnom: "AB1",
+    color: "red",
+  },
+  "Caoimhin Kelleher": {
+    Surnom: "The Irish Shot-Stopper",
+    color: "red",
+  },
+};
 
-  salahButtons.forEach((salahButton) => {
-    salahButton.addEventListener("click", () => {
-      if (salahButton.innerHTML === "Mohamed Salah") {
-        salahButton.innerText = "The Egyptian King";
-        salahButton.style.fontSize = "24px";
-        salahButton.style.background = "black";
-        salahButton.style.color = "red";
-      } else {
-        salahButton.innerText = "Mohamed Salah";
-        salahButton.style.fontSize = "16px";
-        salahButton.style.background = "red";
-        salahButton.style.color = "white";
+const joueurs = [
+  {
+    numero: "11",
+    nom: "Mohamed Salah",
+    poste: "Ailier droit",
+    image: "Images/mohamed-salah-profile-headshot-202425.webp",
+    minutesJouees: "3130",
+    apparitions: "44",
+    buts: "25",
+    passesDecisives: "14",
+  },
+  {
+    numero: "9",
+    nom: "Darwin Nunez",
+    poste: "Buteur",
+    image: "Images/darwin-nunez-profile-headshot-202425.webp",
+    minutesJouees: "3026",
+    apparitions: "54",
+    buts: "18",
+    passesDecisives: "13",
+  },
+  {
+    numero: "18",
+    nom: "Cody Gakpo",
+    poste: "Ailier Droit / Buteur",
+    image: "Images/cody-gakpo-profile-headshot-202425.webp",
+    minutesJouees: "3017",
+    apparitions: "53",
+    buts: "16",
+    passesDecisives: "6",
+  },
+  {
+    numero: "20",
+    nom: "Diogo Jota",
+    poste: "Attaquant",
+    image: "Images/diogo-jota-profile-headshot-202425.webp",
+    minutesJouees: "1710",
+    apparitions: "32",
+    buts: "15",
+    passesDecisives: "4",
+  },
+  {
+    numero: "7",
+    nom: "Luis Diaz",
+    poste: "Ailier",
+    image: "Images/luis-diaz-profile-headshot-202425.webp",
+    minutesJouees: "3615",
+    apparitions: "51",
+    buts: "13",
+    passesDecisives: "5",
+  },
+  {
+    numero: "8",
+    nom: "Dominik Szoboszlai",
+    poste: "Milieu Central",
+    image: "Images/dominik-szoboszlai-profile-headshot-202425.webp",
+    minutesJouees: "2702",
+    apparitions: "45",
+    buts: "7",
+    passesDecisives: "4",
+  },
+  {
+    numero: "10",
+    nom: "Alexis Mac Allister",
+    poste: "Milieu Central",
+    image: "Images/alexis-mac-allister-profile-headshot-202425.webp",
+    minutesJouees: "3498",
+    apparitions: "46",
+    buts: "7",
+    passesDecisives: "7",
+  },
+  {
+    numero: "3",
+    nom: "Wataru Endo",
+    poste: "Milieu Défensif",
+    image: "Images/wataru-endo-profile-headshot-202425.webp",
+    minutesJouees: "2757",
+    apparitions: "43",
+    buts: "3",
+    passesDecisives: "13",
+  },
+  {
+    numero: "19",
+    nom: "Harvey Elliott",
+    poste: "Milieu Offensif",
+    image: "Images/harvey-elliott-profile-headshot-202425.webp",
+    minutesJouees: "2787",
+    apparitions: "53",
+    buts: "4",
+    passesDecisives: "11",
+  },
+  {
+    numero: "38",
+    nom: "Ryan Gravenberch",
+    poste: "Milieu Central",
+    image: "Images/ryan-gravenberch-profile-headshot-202425.webp",
+    minutesJouees: "1838",
+    apparitions: "38",
+    buts: "4",
+    passesDecisives: "2",
+  },
+  {
+    numero: "2",
+    nom: "Joe Gomez",
+    poste: "Défenseur Central",
+    image: "Images/joe-gomez-profile-headshot-202425.webp",
+    minutesJouees: "3136",
+    apparitions: "51",
+    buts: "0",
+    passesDecisives: "0",
+  },
+  {
+    numero: "4",
+    nom: "Virgil van Dijk",
+    poste: "Défenseur Central",
+    image: "Images/virgil-van-dijk-profile-headshot-202425.webp",
+    minutesJouees: "4072",
+    apparitions: "48",
+    buts: "0",
+    passesDecisives: "0",
+  },
+  {
+    numero: "5",
+    nom: "Ibrahima Konate",
+    poste: "Défenseur Central",
+    image: "Images/ibrahima-konate-headshot-202425.webp",
+    minutesJouees: "2586",
+    apparitions: "37",
+    buts: "0",
+    passesDecisives: "0",
+  },
+  {
+    numero: "21",
+    nom: "Kostas Tsimikas",
+    poste: "Latéral Gauche",
+    image: "Images/kostas-tsimikas-headshot-202425.webp",
+    minutesJouees: "1464",
+    apparitions: "25",
+    buts: "0",
+    passesDecisives: "4",
+  },
+  {
+    numero: "26",
+    nom: "Andy Robertson",
+    poste: "Latéral Gauche",
+    image: "Images/andy-robertson-headshot-202425.webp",
+    minutesJouees: "2193",
+    apparitions: "30",
+    buts: "3",
+    passesDecisives: "2",
+  },
+  {
+    numero: "66",
+    nom: "Trent Alexander-Arnold",
+    poste: "Latéral Droit",
+    image: "Images/trent-alexander-arnold-profile-headshot-202425.webp",
+    minutesJouees: "2613",
+    apparitions: "37",
+    buts: "3",
+    passesDecisives: "9",
+  },
+  {
+    numero: "78",
+    nom: "Jarell Quansah",
+    poste: "Défenseur Central",
+    image: "Images/jarell-quansah-profile-headshot-202425.webp",
+    minutesJouees: "2493",
+    apparitions: "33",
+    buts: "0",
+    passesDecisives: "0",
+  },
+  {
+    numero: "84",
+    nom: "Conor Bradley",
+    poste: "Latéral Droit",
+    image: "Images/conor-bradley-profile-headshot-202425.webp",
+    minutesJouees: "1506",
+    apparitions: "23",
+    buts: "1",
+    passesDecisives: "6",
+  },
+  {
+    numero: "1",
+    nom: "Alisson Becker",
+    poste: "Gardien de but",
+    image: "Images/alisson-becker-profile-headshot-202425.webp",
+    minutesJouees: "3130",
+    apparitions: "32",
+    buts: "0",
+    passesDecisives: "0",
+  },
+  {
+    numero: "62",
+    nom: "Caoimhin Kelleher",
+    poste: "Gardien de but",
+    image: "Images/caoimhin-kelleher-profile-headshot-202425.webp",
+    minutesJouees: "2400",
+    apparitions: "26",
+    buts: "0",
+    passesDecisives: "0",
+  },
+];
+
+function genererCarteJoueur(joueur) {
+  return `
+    <div class="swiper-slide">
+      <div class="cartejoueur1">
+        <div class="presentationjoueur">
+          <div class="textejoueur">
+            <p>${joueur.numero}</p>
+            <p>${joueur.nom}</p>
+            <p>${joueur.poste}</p>
+          </div>
+          <div class="imagejoueur">
+            <img src="${joueur.image}" alt="${joueur.nom}" />
+          </div>
+        </div>
+        <div class="stats">
+          <p>Saison 2023/2024</p>
+        </div>
+        <div class="container">
+          <div class="tempsjoue">
+            <p>Minutes jouées</p>
+            <p>${joueur.minutesJouees}</p>
+          </div>
+        </div>
+        <div class="container">
+          <div class="apparition">
+            <p>Apparition</p>
+            <p>${joueur.apparitions}</p>
+          </div>
+        </div>
+        <div class="container">
+          <div class="buts">
+            <p>Buts</p>
+            <p>${joueur.buts}</p>
+          </div>
+        </div>
+        <div class="container">
+          <div class="passe-d">
+            <p>Passes décisives</p>
+            <p>${joueur.passesDecisives}</p>
+          </div>
+        </div>
+        <div class="buttoncarte">
+          <button class="js-button" data-joueur-name="${joueur.nom}">${joueur.nom}</button>
+        </div>
+      </div>
+    </div>
+  `;
+}
+
+function genererToutesLesCartes() {
+  const conteneurCartes = document.querySelector(".swiper-wrapper");
+  conteneurCartes.innerHTML = joueurs.map(genererCarteJoueur).join("");
+}
+
+function initialiserBoutons() {
+  const boutonJoueurs = document.querySelectorAll(".js-button");
+
+  boutonJoueurs.forEach((bouton) => {
+    bouton.addEventListener("click", () => {
+      const NomJoueurs = bouton.getAttribute("data-joueur-name");
+      const Joueur = JoueurInfo[NomJoueurs];
+
+      if (Joueur) {
+        if (bouton.classList.contains("active")) {
+          // Retour à l'état initial
+          bouton.innerText = NomJoueurs;
+          bouton.style.fontSize = "20px";
+          bouton.style.background = "red";
+          bouton.style.color = "white";
+          bouton.classList.remove("active");
+        } else {
+          // Changement vers le surnom
+          bouton.innerText = Joueur.Surnom;
+          bouton.style.fontSize = "24px";
+          bouton.style.background = "black";
+          bouton.style.color = Joueur.color;
+          bouton.classList.add("active");
+        }
       }
     });
   });
-});
+}
 
-const swiper = new Swiper(".swiper", {
-  pagination: {
-    el: ".swiper-pagination",
-    clickable: true,
-  },
+document.addEventListener("DOMContentLoaded", () => {
+  genererToutesLesCartes();
+  initialiserBoutons();
 
-  navigation: {
-    nextEl: ".swiper-button-next",
-    prevEl: ".swiper-button-prev",
-  },
-
-  slidesPerView: 3,
-  loop: true,
-
-  autoplay: {
-    delay: 1600,
-    disableOnInteraction: false,
-    pauseOnMouseEnter: true,
-  },
-  speed: 700,
-  breakpoints: {
-    320: {
-      slidesPerView: 1,
-      spaceBetween: 10,
+  const swiper = new Swiper(".swiper", {
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
     },
-    480: {
-      slidesPerView: 1,
-      spaceBetween: 20,
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
     },
-    640: {
-      slidesPerView: 1,
-      spaceBetween: 30,
+    slidesPerView: 3,
+    loop: true,
+    autoplay: {
+      delay: 2200,
+      disableOnInteraction: false,
+      pauseOnMouseEnter: true,
     },
-    1024: {
-      slidesPerView: 3,
-      spaceBetween: 40,
+    speed: 700,
+    breakpoints: {
+      320: { slidesPerView: 1, spaceBetween: 10 },
+      480: { slidesPerView: 1, spaceBetween: 20 },
+      640: { slidesPerView: 1, spaceBetween: 30 },
+      1024: { slidesPerView: 3, spaceBetween: 40 },
+      1366: { slidesPerView: 3, spaceBetween: 50 },
     },
-    1366: {
-      slidesPerView: 3,
-      spaceBetween: 50,
-    },
-  },
+  });
 });
